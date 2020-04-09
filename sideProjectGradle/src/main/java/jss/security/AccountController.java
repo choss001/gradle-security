fd816f6c884f1ac280defc0b37a511346bb22711
JSS <choss01@oneplat.co>
2020-04-09 14:27:19 +0900

    Thymeleaf 성공해버려짜너

--- sideProjectGradle/src/main/java/jss/security/AccountController.java
+++ sideProjectGradle/src/main/java/jss/security/AccountController.java
@@ -1,15 +1,16 @@
 package jss.security;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AccountController {
 	
 	
 	@RequestMapping("/hello")
 	public String login() {
-		return "hello";
+		return "index";
 	}	
 
 }
---
+++ sideProjectGradle/src/main/java/jss/thymeleaf/ThymeleafController.java
@@ -0,0 +1,16 @@
+package jss.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ThymeleafController{
+
+@RequestMapping("/")
+public String index(Model model) {
+    model.addAttribute("message", "HELLO!¹¹Áö");
+    return "index";
+}
+
+}
